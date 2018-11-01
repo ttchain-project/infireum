5d23be8982b6bfbd46677475203dec58a8ecf249
Ajinkya.Sharma <ajinkya.sharma@git4u.com>
2018-11-01 20:55:28 +0800

    Added a trial nav bar and images

---
+++ TTChain/Util/View Controller Modulairization/AJNavigationController.swift
@@ -0,0 +1,30 @@
+//
+//  AJNavigatoinController.swift
+//  TTChain
+//
+//  Created by Ajinkya Sharma on 2018/11/1.
+//  Copyright © 2018 gib. All rights reserved.
+//
+
+import UIKit
+
+class AJNavigatoinController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
