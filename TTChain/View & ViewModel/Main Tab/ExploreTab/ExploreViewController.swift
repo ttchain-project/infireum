3d801df02b4a7e1be3d4aff2a9a9b71bbffd3a02
Ajinkya.Sharma <ajinkya.sharma@git4u.com>
2018-11-05 18:32:24 +0800

    Images added

---
+++ TTChain/View & ViewModel/Main Tab/ExploreTab/ExploreViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ExploreViewController.swift
+//  TTChain
+//
+//  Created by Ajinkya Sharma on 2018/11/5.
+//  Copyright © 2018 gib. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    
+    
+    @IBOutlet weak var friendsTitleLabel: UILabel!
+    @IBOutlet weak var friendsSepLine: UIImageView!
+    @IBOutlet weak var newsLabel: UILabel!
+    @IBOutlet weak var newsSepLine: UIImageView!
+    @IBOutlet weak var videosLabel: UILabel!
+    @IBOutlet weak var videosSepLine: UIImageView!
+    @IBOutlet weak var dappLabel: UILabel!
+    @IBOutlet weak var dappSepLine: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
---
+++ TTChain/View & ViewModel/Main Tab/ExploreTab/ViewModel/ExploreTabViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  ExploreTabViewModel.swift
+//  TTChain
+//
+//  Created by Ajinkya Sharma on 2018/11/5.
+//  Copyright © 2018 gib. All rights reserved.
+//
+
+import Foundation
