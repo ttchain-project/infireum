a5f0930731da142309eb1adc655e1156b3b8eb67
Ajinkya.Sharma <ajinkya.sharma@git4u.com>
2019-03-22 16:33:21 +0800

    Create Red envelope vc

---
+++ TTChain/View & ViewModel/Main Tab/RedEnvelope/CreateRedEnvelopeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreateRedEnvelopeViewController.swift
+//  TTChain
+//
+//  Created by Ajinkya Sharma on 2019/3/22.
+//  Copyright © 2019 gib. All rights reserved.
+//
+
+import UIKit
+
+class CreateRedEnvelopeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
