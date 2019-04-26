c7c80ff2c56c50a410300110f93a0e35bf9799c6
Ajinkya.Sharma <ajinkya.sharma@git4u.com>
2019-04-26 20:18:03 +0800

    Check for block greater than 0 to mark trans as processing

--- TTChain/View & ViewModel/Main Tab/Wallet/Asset Detail/Tab/Record/RecordDetail/TransRecordDetailViewController.swift
+++ TTChain/View & ViewModel/Main Tab/Wallet/Asset Detail/Tab/Record/RecordDetail/TransRecordDetailViewController.swift
@@ -60,7 +60,7 @@ final class TransRecordDetailViewController: KLModuleViewController,KLInstanceSe
             case .withdrawal:
                 amtStr = "-"
                 if constructor.asset.wallet?.walletMainCoinID == Coin.btc_identifier {
-                    if transRecord.block != 0 {
+                    if transRecord.block > 0 {
                         transAmount = transAmount?.subtracting(transRecord.totalFee ?? NSDecimalNumber.init(value:0.0))
                     }
                 }
