c939f5fea91d38c46a10cc11c858cba2cdc623fd
Ajinkya.Sharma <ajinkya.sharma@git4u.com>
2019-06-20 16:21:22 +0800

    Chat controller added.

---
+++ TTChain/View & ViewModel/Main Tab/ChatTab/ChatContainerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChatContainerViewController.swift
+//  TTChain
+//
+//  Created by Ajinkya Sharma on 2019/6/20.
+//  Copyright © 2019 gib. All rights reserved.
+//
+
+import UIKit
+
+class ChatContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
